4ee44dbf1be820db16b5fd3056c6d8ced9630559
kazacoder <avtk.tlt@gmail.com>
2024-04-24 21:48:21 +0300

    day 6 - 3

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -11,7 +11,7 @@ def set_price(subs_id):
 
     time.sleep(5)
 
-    subcription = Subscription.objects.filter(id=subs_id).annotate(
+    subscription = Subscription.objects.filter(id=subs_id).annotate(
         annotated_price=F('service__full_price') * (1 - F('plan__discount_percent') / 100.00)).first()
-    subcription.price = subcription.annotated_price
-    subcription.save()  # (save_model=False)
+    subscription.price = subscription.annotated_price
+    subscription.save()  # (save_model=False)
