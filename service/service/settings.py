f932a10946c84b21804d048c22d86fe624af1335
kazacoder <avtk.tlt@gmail.com>
2024-04-24 11:37:51 +0300

    day 3-2

--- service/service/settings.py
+++ service/service/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGGING= {
+LOGGING = {
     'version': 1,
     'handlers': {
         'console': {'class': 'logging.StreamHandler'}
