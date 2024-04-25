1c6a8aa06598418d898a267448319a546e01346f
kazacoder <avtk.tlt@gmail.com>
2024-04-25 22:06:35 +0300

    day 8 - 0.1

--- service/services/views.py
+++ service/services/views.py
@@ -1,5 +1,7 @@
 from django.db.models import Prefetch, F, Sum
 from django.shortcuts import render
+from django.core.cache import cache
+from django.conf import settings
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from clients.models import Client
@@ -18,7 +20,15 @@ class SubscriptionView(ReadOnlyModelViewSet):
         queryset = self.filter_queryset(self.get_queryset())
         response = super().list(request, *args, **kwargs)
 
+
+        price_cache = cache.get(settings.PRICE_CACHE_NAME)
+
+        if price_cache:
+            total_price = price_cache
+        else:
+            total_price = queryset.aggregate(total=Sum('price')).get('total')
+            cache.set(settings.PRICE_CACHE_NAME, total_price, 30)
         response_data = {'result': response.data}
-        response_data['total'] = queryset.aggregate(total=Sum('price')).get('total')
+        response_data['total'] = total_price
         response.data = response_data
         return response
